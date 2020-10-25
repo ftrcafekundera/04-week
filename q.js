a9189298e79ac45a66bcc971549865074be08570
ftrcafekundera <71603259+ftrcafekundera@users.noreply.github.com>
2020-10-25 00:47:20 -0400

    Added the questions for the quiz

---
+++ q.js
@@ -0,0 +1,22 @@
+var questions = [
+    {
+        title: "What distinguishes JavaScript from HTML and CSS?",
+        choices:["Its name is different.", "It works on specific devices.", "It makes a webpage or application dynamic.", "Nothing."],
+        answer: "It makes a webpage or application dynamic."
+    },
+    {
+        title: "Who invented JavaScript?",
+        choices:["Brendan Eich", "Hakon Wium Lie", "Tim Berners Lee", "Jennifer Java"],
+        answer: "Brendan Eich"
+    },
+    {
+        title: "Which tag connects JavaScript to HTML?",
+        choices:["Style Tag", "Script Tag", "Bootstrap", "It connects automatically."],
+        answer: "Script Tag"
+    },
+    {
+        title: "Which of the answers below is correct?",
+        choices:["You can only connect one JavaScript file to an HTML file.","JavaScript is not helpful when creating dynamic, responsive webpages.", "JavaScript can operate independently from HTML and CSS.", "You can style a webpage with JavaScript."],
+        answer: "You can style a webpage with JavaScript."
+    }
+]
